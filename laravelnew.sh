e3cbe2b5f7b18674d9a3a24125cceabb6c7689ab
Michael Stokoe <mstokoe0990@gmail.com>
2019-07-19 20:20:20 +0100

    Update laravelnew.sh

--- laravelnew.sh
+++ laravelnew.sh
@@ -4,169 +4,133 @@
 # Either copy & paste the full function into your .bashrc (or similar)
 # OR save this file as laravelnew.sh and add source ~/laravelnew.sh (or wherever it was saved to) to your .bashrc (or similar)
 
-function laravelnew () {
-    # New Project Setup.
-    # 
-    # Copies .env, replaces APP_NAME, APP_URL, DB_HOST, DB_DATABASE and REDIS_HOST
-    # You may need to change the variables below to account for your own setup.
-    #
-    # It is important that you keep your projects under ~/code or ~/projects.
-    # This script will detect which you're using. (If you have both, it will place the new project into ~/projects)
+# New Project Setup.
+# 
+# Copies .env, replaces APP_NAME, APP_URL, DB_HOST, DB_DATABASE and REDIS_HOST
+# You may need to change the variables below to account for your own setup.
+#
+# It is important that you keep your projects under ~/code or ~/projects.
+# This script will detect which you're using. (If you have both, it will place the new project into ~/projects)
 
-    # IP address you access homestead through. This is defined in homestead.yaml
-    homesteadip=192.168.10.10
-    
-    # Top-level Domain: ex. http://app.test
-    tld=test
-    
-    # Homestead directory
-    hsdir=~/Homestead
-    
-    # Homestead.yaml location
-    hsyaml=~/Homestead/Homestead.yaml
+# IP address you access homestead through. This is defined in homestead.yaml
+homesteadip=192.168.10.10
 
-    # Database password
-    dbpassword=secret
-    
-    echo "Finding projects directory..."
-    
-    if [ -d "~/code" ]; then
-        echo "Found projects under 'code' directory"
-        cd ~/code
-    fi
+# Top-level Domain: ex. http://app.test
+tld=test
 
-    if [ -d "~/projects" ]; then
-        echo "Found projects under 'projects' directory"
-        cd ~/projects
-    fi
+# Homestead directory
+hsdir=~/Homestead
 
-    if ! [ command -v laravel ]; then
-        echo "'laravel' command doesn't seem to be in your PATH"
-        echo "Add export PATH=$PATH:~/.config/composer/vendor/bin to your ~/.bashrc or similar"
-        echo "...and make sure you've ran composer global install laravel/installer"
-        cd -
-        return 0
-    fi
+# Homestead.yaml location
+hsyaml=~/Homestead/Homestead.yaml
 
-    if ! [ command -v composer ]; then
-        echo "Please install composer first."
-        return 0
-    fi
+# Database user
+dbuser=homestead
+# Database password
+dbpassword=secret
 
-    echo "Finding (js) package manager..."
-    packagemanager=yarn
-    if ! [ command -v yarn ]; then
-        echo "Yarn is not installed, checking for npm..."
-        if ! [ command -v npm ]; then
-            echo "npm isn't installed either. Exiting."
-            return 0
-        fi
-        packagemanager=npm
-    fi
+# Package manager (JS) "yarn" or "npm"
+packagemanager=npm
 
-    projectname=$1
+echo "Finding projects directory..."
 
-    echo "Creating new Laravel project called $projectname"
-    laravel new $projectname
+if [ -d "/home/$USER/code" ]
+then
+    echo "Found projects under 'code' directory"
+    cd /home/$USER/code
+fi
 
-    cd ./$projectname
+if [ -d "/home/$USER/projects" ]
+then
+    echo "Found projects under 'projects' directory"
+    cd /home/$USER/projects
+fi
 
-    # Current directory name. (To use as APP_NAME, APP_URL and MySQL DB name).
+projectname=$1
 
-    if ! [ -f .env ]; then
-        if ! [ -f .env.example ]; then
-            echo "Please make sure .env.example exists."
-            return 0
-        fi
-        
-        echo "Cloning .env.example into .env."
-        cp .env.example .env
-    fi
+echo "Creating new Laravel project called $projectname"
+laravel new $projectname 1>/dev/null
 
-    sed -i "s/APP_NAME=Laravel/APP_NAME='$projectname'/g" .env
-    sed -i "s/APP_URL=http:\/\/localhost/APP_URL=http:\/\/$projectname.$tld/g" .env
-    sed -i "s/DB_HOST=127\.0\.0\.1/DB_HOST=$homesteadip/g" .env
-    sed -i "s/DB_DATABASE=homestead/DB_DATABASE='$projectname'/g" .env
-    sed -i "s/DB_PASSWORD=/DB_PASSWORD='$dbpassword'/g" .env
-    sed -i "s/REDIS_HOST=127\.0\.0\.1/REDIS_HOST=$homesteadip/g" .env
+cd ./$projectname
 
-    # Comment out the following if you don't use /etc/hosts
-    if grep -q "$homesteadip $projectname.$tld" /etc/hosts; then
-        echo "Hosts entry didn't exist for application."
-        echo "Adding hosts entry for this application."
-        echo "\n$homesteadip $projectname.$tld" | sudo tee -a /etc/hosts
-        echo "Hosts entry added. Your app will be live at http://$projectname.$tld"
-    fi
+# Change your editor here if you don't use VS Code.
+code -g ./
+xdg-open ./ 1>/dev/null
 
-    # Comment out the following if you want to write the homestead.yaml entries yourself.
-    if grep -q "$projectname.$tld" $hsyaml; then
-        echo "No entries in $hsyaml for this application, adding them now."
-        sed -i "/^sites:/a \ \ \ \ - map: $projectname.$tld" $hsyaml
-        sed -i "/^\ \ \ \ - map: $projectname.$tld/a \ \ \ \ \ \ to: /home/vagrant/projects/$projectname/public \n" $hsyaml
-        echo "Adding database with name $projectname to $hsyaml"
-        sed -i "/^databases:/a \ \ \ \ - $projectname" $hsyaml
-    fi
-    
-    echo "Running $packagemanager & composer"
+sed -i "s/APP_NAME=Laravel/APP_NAME='$projectname'/g" .env
+sed -i "s/APP_URL=http:\/\/localhost/APP_URL=http:\/\/$projectname.$tld/g" .env
+sed -i "s/DB_HOST=127\.0\.0\.1/DB_HOST=$homesteadip/g" .env
+sed -i "s/DB_DATABASE=homestead/DB_DATABASE='$projectname'/g" .env
+sed -i "s/DB_USERNAME=root/DB_USERNAME='$dbuser'/g" .env
+sed -i "s/DB_PASSWORD=/DB_PASSWORD='$dbpassword'/g" .env
+sed -i "s/REDIS_HOST=127\.0\.0\.1/REDIS_HOST=$homesteadip/g" .env
 
-    if [ "$packagemanager" = "yarn" ]; then
-        yarn
-    elif [ "$packagemanager" = "npm" ]; then
-        npm install
-    else
-        echo "$packagemanager isn't a known package manager. Moving on."
-    fi
+# Comment out the following if you don't use /etc/hosts
+if ! [ grep "$homesteadip $projectname.$tld" /etc/hosts ]
+then
+    echo "Hosts entry didn't exist for application."
+    echo "Adding hosts entry for this application."
+    echo "$homesteadip $projectname.$tld" | sudo tee -a /etc/hosts
+    echo "Hosts entry added. Your app will be live at http://$projectname.$tld"
+fi
 
-    composer install
+# Comment out the following if you want to write the homestead.yaml entries yourself.
+if ! [ grep "$projectname.$tld" $hsyaml ]
+then
+    echo "No entries in $hsyaml for this application, adding them now."
+    sed -i "/^sites:/a \ \ \ \ - map: $projectname.$tld" $hsyaml
+    sed -i "/^\ \ \ \ - map: $projectname.$tld/a \ \ \ \ \ \ to: /home/vagrant/projects/$projectname/public" $hsyaml
+    echo "Adding database with name $projectname to $hsyaml"
+    sed -i "/^databases:/a \ \ \ \ - $projectname" $hsyaml
+fi
 
-    if ! [ -f artisan ]; then
-        echo "Artisan not detected. Exiting."
-        return 0
-    fi
+echo "Running $packagemanager & composer"
 
-    echo "Generating App Key"
-    php artisan key:generate
-    
-    echo "Installing extra dependencies..."
-    echo "Installing itsgoingd/clockwork"
-    composer require --dev itsgoingd/clockwork
-    echo "Installing barryvdh/laravel-ide-helper"
-    composer require --dev barryvdh/laravel-ide-helper
-    echo "Generating ide helper files..."
-    php artisan ide-helper:generate
-    echo "Installing protoqol/prequel"
-    composer require --dev protoqol/prequel
-    php artisan prequel:install
-    echo "Installing spatie/laravel-permission"
-    composer require spatie/laravel-permission
-    echo "Publishing assets..."
-    php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --tag="migrations"
-    php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --tag="config"
+if [ "$packagemanager" = "yarn" ]
+then
+    yarn 1>/dev/null
+elif [ "$packagemanager" = "npm" ]
+then
+    npm install 1>/dev/null
+else
+    echo "$packagemanager isn't a known package manager. Moving on."
+fi
+
+if ! [ -f artisan ]
+then
+    echo "Artisan not detected. Exiting."
+    return 0
+fi
+
+echo "Generating App Key"
+php artisan key:generate
+
+echo "Installing extra dependencies..."
+composer require --dev itsgoingd/clockwork
+composer require --dev barryvdh/laravel-ide-helper
+php artisan ide-helper:generate
+composer require --dev protoqol/prequel
+php artisan prequel:install
+composer require spatie/laravel-permission
+echo "Publishing assets..."
+php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --tag="migrations"
+php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider" --tag="config"
 
 
-    # Comment out these 3 lines if you DO NOT wish to reprovision your vagrant box after doing this setup.
-    echo "Reprovisioning Vagrant Box"
-    cd $hsdir
-    vagrant up --provision
-    cd -
-    
-    # Perhaps not entirely useful on a new project, but at least it makes sure you've set up the scripts correctly.
-    echo "Running database migrations"
-    php artisan migrate:fresh --force --seed
+# Comment out these 3 lines if you DO NOT wish to reprovision your vagrant box after doing this setup.
+echo "Reprovisioning Vagrant Box"
+cd $hsdir
+vagrant up --provision 1>/dev/null
+echo "Finished provision."
+cd -
 
-    if [ command -v pstorm ]; then
-        pstorm ./
-    fi
+# Perhaps not entirely useful on a new project, but at least it makes sure you've set up the scripts correctly.
+echo "Running database migrations"
+php artisan migrate:fresh --force --seed  1>/dev/null
+echo "Migrated."
 
-    if [ command -v code ]; then
-        code -g ./
-    fi
-
-    xdg-open ./
-
-    echo "ALL DONE!"
-    echo "Find your app online at:"
-    echo "https://$projectname.$tld"
-    echo "or"
-    echo "http://$projectname.$tld"
-}
+echo "ALL DONE!"
+echo "Find your app online at:"
+echo "https://$projectname.$tld"
+echo "or"
+echo "http://$projectname.$tld"
